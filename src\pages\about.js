ecbe4e22355662346657fe71cd4d0b9a591c3b84
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 19:27:49 +0500

    Add src\pages\about.js

---
+++ src\pages\about.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>About UNICEF</h1>
+        <p>UNICEF is the world's largest provider of vaccines for developing countries...</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default AboutPage;
