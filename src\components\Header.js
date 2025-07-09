ed6ae2dd7d8d83e291d551fd58c2eb7e2ee3a6eb
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 19:27:47 +0500

    Add src\components\Header.js

---
+++ src\components\Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Donate</a></li>
+          <li><a href="#">Get Involved</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
